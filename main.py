abfe9927e457f6505faccfcddc62f22b0e2c2df8
jcatrambone94 <jcatrambone@damglorious.ninja>
2016-07-24 19:19:56 -0400

    Update main.py
    Usability

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 import aes_dec
 import aes_enc
+import sys
 
-encrypter = aes_enc.AES_Enc(verbose=False)
-decrypter = aes_dec.AES_Dec(verbose=False)
 
-encrypter.encrypt("password", 'rockyou.txt', 'test')
-decrypter.decrypt("password", 'test','test.txt')
+Usage = "Usage: python Damglorious-Encryption.py <e/d> <Password> <Input Path> <Output Path>"
+
+
+
+if len(sys.argv) < 4:
+	print Usage
+	sys.exit(0)
+
+else:
+	EncOrDec = sys.argv[1]
+	password = sys.argv[2]
+	inputPath = sys.argv[3]
+	outputPath = sys.argv[4]
+
+	if EncOrDec == "e":
+		encrypter = aes_enc.AES_Enc(verbose=False)
+		encrypter.encrypt(password,inputPath,outputPath)
+
+	elif EncOrDec == 'd':
+		decrypter = aes_dec.AES_Dec(verbose=False)
+		decrypter.decrypt(password,inputPath,outputPath)
+
+	else:
+		print Usage
